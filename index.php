612ee5308f2a79d6da605c989424c1c1827ed3e0
GabrieleGrossi <grossigabriele98@gmail.com>
2023-07-13 14:47:00 +0200

    try to add the first Movie

--- index.php
+++ index.php
@@ -9,8 +9,20 @@ vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori 
 <?php
     class Movie{
         //qui vanno le caratteristiche
+        public $movieName;
+        public $movieMainActor;
+        public $movieYear;
+        public $movieDirector;
 
 
         //qui vanno i metodi
+        function __construct($movieName, $movieMainActor, $movieYear, $movieDirector){
+            $this->movieName =$movieName;
+            $this->movieMainActor=$movieMainActor;
+            $this->movieYear=$movieYear;
+            $this->movieDirector=$movieDirector;
+        }
     }
+    $HarryPotter = new Movie("Harry Potter e la pietra filosofale", "Daniel Radcliff", "2001", "Chris Columbus");
+    echo $HarryPotter->movieName;
 ?>
