356ec8e3c6911240a63d10356761a1b6b1718f18
GabrieleGrossi <grossigabriele98@gmail.com>
2023-07-13 15:06:04 +0200

    exercise done

--- index.php
+++ index.php
@@ -22,10 +22,82 @@ vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori 
             $this->movieYear=$movieYear;
             $this->movieDirector=$movieDirector;
         }
+
+        function name(){
+            return $this->movieName;
+        }
+        function attore(){
+            return $this->movieMainActor;
+        }
+        function anno(){
+            return $this->movieYear;
+        }
+        function regista(){
+            return $this->movieDirector;
+        }
     }
     $HarryPotter = new Movie("Harry Potter e la pietra filosofale", "Daniel Radcliff", "2001", "Chris Columbus");
-    echo $HarryPotter->movieName;
-    echo $HarryPotter->movieMainActor;
-    echo $HarryPotter->movieYear;
-    echo $HarryPotter->movieDirector;
+    $HarryPotter2 = new Movie("Harry Potter e la camera dei segreti", "Daniel Radcliff", "2002", "Chris Columbus");
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+</head>
+<body>
+    <h1>
+        Lista dei film più belli di sempre
+    </h1>
+    <h2>
+        Titolo: 
+        <?php
+        echo $HarryPotter->name();
+        ?>
+    </h2>
+    <h2>
+        Attore protagonista: 
+        <?php
+        echo $HarryPotter->attore();
+        ?>
+    </h2>
+    <h2>
+        Anno di uscita: 
+        <?php
+        echo $HarryPotter->anno();
+        ?>
+    </h2>
+    <h2>
+        Diretto da: 
+        <?php
+        echo $HarryPotter->regista();
+        ?>
+    </h2>
+
+    <h2>
+        Titolo: 
+        <?php
+        echo $HarryPotter2->name();
+        ?>
+    </h2>
+    <h2>
+        Attore protagonista: 
+        <?php
+        echo $HarryPotter2->attore();
+        ?>
+    </h2>
+    <h2>
+        Anno di uscita: 
+        <?php
+        echo $HarryPotter2->anno();
+        ?>
+    </h2>
+    <h2>
+        Diretto da: 
+        <?php
+        echo $HarryPotter2->regista();
+        ?>
+    </h2>
+</body>
+</html>
